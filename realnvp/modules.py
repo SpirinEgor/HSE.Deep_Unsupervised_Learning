b324f13d5f03ce5d4a761e31b2933eafdfb461f0
Egor Spirin <spirin.egor@mail.ru>
2022-03-09 01:31:01 +0500

    Implemented RealNVP

---
+++ realnvp/modules.py
@@ -0,0 +1,195 @@
+from typing import Tuple
+
+import torch
+from torch import nn
+
+
+class NormalizedConv2D(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0):
+        super().__init__()
+        self.conv = nn.utils.weight_norm(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding))
+
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
+        return self.conv(batch)
+
+
+class ActNorm(nn.Module):
+    def __init__(self, n_channels: int):
+        super().__init__()
+        self.n_channels = n_channels
+
+        self.log_s = nn.Parameter(torch.zeros(1, n_channels, 1, 1), requires_grad=True)
+        self.b = nn.Parameter(torch.zeros(1, n_channels, 1, 1), requires_grad=True)
+        self._initialized = False
+
+    def forward(self, batch: torch.Tensor, reverse: bool = False) -> Tuple[torch.Tensor, torch.Tensor]:
+        if reverse:
+            return (batch - self.b) * torch.exp(-self.log_s), self.log_s
+
+        if not self._initialized:
+            self.b.data = -torch.mean(batch, dim=(0, 2, 3), keepdim=True)
+            s = torch.std(batch.permute(1, 0, 2, 3).reshape(self.n_channels, -1), dim=1).reshape(
+                1, self.n_channels, 1, 1
+            )
+            self.log_s.data = -torch.log(s)
+
+        return batch * self.log_s.exp() + self.b, self.log_s
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, n_filters: int):
+        super().__init__()
+        self.block = nn.Sequential(
+            NormalizedConv2D(n_filters, n_filters, 1, 1, 0),
+            nn.ReLU(),
+            NormalizedConv2D(n_filters, n_filters, 3, 1, 1),
+            nn.ReLU(),
+            NormalizedConv2D(n_filters, n_filters, 1, 1, 0),
+        )
+
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
+        return batch + self.block(batch)
+
+
+class AffineCouplingWithCheckerboard(nn.Module):
+    def __init__(self, top_left: int = 1, img_sz: int = 32, n_blocks: int = 4, hidden_ch: int = 128):
+        super().__init__()
+        self.mask = torch.arange(img_sz) + torch.arange(img_sz).reshape(-1, 1)
+        self.mask = torch.remainder(top_left + self.mask, 2)
+        self.mask = self.mask.reshape(1, 1, img_sz, img_sz).float()
+        self.mask = nn.Parameter(self.mask, requires_grad=False)
+
+        self.g = nn.Parameter(torch.zeros(1), requires_grad=True)
+        self.b = nn.Parameter(torch.zeros(1), requires_grad=True)
+
+        layers = [NormalizedConv2D(3, hidden_ch, 3, 1, 1)]
+        for _ in range(n_blocks):
+            layers.append(ResidualBlock(hidden_ch))
+        layers += [nn.ReLU(), NormalizedConv2D(hidden_ch, 6, 3, 1, 1)]
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, batch: torch.Tensor, reverse: bool = False) -> Tuple[torch.Tensor, torch.Tensor]:
+        bs, n_channels, *_ = batch.shape
+        mask = self.mask.repeat(bs, 1, 1, 1)
+
+        masked_batch = batch * mask
+
+        log_s, b = self.net(masked_batch).split(n_channels, dim=1)
+        log_s = self.g * torch.tanh(log_s) + self.b
+
+        b = b * (1 - mask)
+        log_s = log_s * (1 - mask)
+
+        x = ((batch - b) * torch.exp(-log_s)) if reverse else (batch * log_s.exp() + b)
+        return x, log_s
+
+
+class AffineCouplingWithChannels(nn.Module):
+    def __init__(self, is_top_left: bool = True, n_blocks: int = 4, hidden_ch: int = 128):
+        super().__init__()
+        self.is_top_left = is_top_left
+
+        self.g = nn.Parameter(torch.zeros(1), requires_grad=True)
+        self.b = nn.Parameter(torch.zeros(1), requires_grad=True)
+
+        layers = [NormalizedConv2D(6, hidden_ch, 3, 1, 1)]
+        for _ in range(n_blocks):
+            layers.append(ResidualBlock(hidden_ch))
+        layers += [nn.ReLU(), NormalizedConv2D(hidden_ch, 12, 3, 1, 1)]
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, batch: torch.Tensor, reverse: bool = False) -> Tuple[torch.Tensor, torch.Tensor]:
+        bs, n_channels, *_ = batch.shape
+
+        on, off = batch.split(n_channels // 2, dim=1)
+        if not self.is_top_left:
+            on, off = off, on
+
+        log_s, b = self.net(off).split(n_channels // 2, dim=1)
+        log_s = self.g * torch.tanh(log_s) + self.b
+
+        on = ((on - b) * torch.exp(-log_s)) if reverse else on * log_s.exp() + b
+
+        if self.is_top_left:
+            return torch.cat([on, off], dim=1), torch.cat([log_s, torch.zeros_like(log_s)], dim=1)
+        else:
+            return torch.cat([off, on], dim=1), torch.cat([torch.zeros_like(log_s), log_s], dim=1)
+
+
+class RealNVP(nn.Module):
+    def __init__(self, hidden_dim: int = 128):
+        super().__init__()
+        self.checker_transforms = [
+            nn.ModuleList(
+                [
+                    AffineCouplingWithCheckerboard(1, hidden_ch=hidden_dim),
+                    ActNorm(3),
+                    AffineCouplingWithCheckerboard(0, hidden_ch=hidden_dim),
+                    ActNorm(3),
+                    AffineCouplingWithCheckerboard(1, hidden_ch=hidden_dim),
+                    ActNorm(3),
+                    AffineCouplingWithCheckerboard(0, hidden_ch=hidden_dim),
+                ]
+            ),
+            nn.ModuleList(
+                [
+                    AffineCouplingWithCheckerboard(1, hidden_ch=hidden_dim),
+                    ActNorm(3),
+                    AffineCouplingWithCheckerboard(0, hidden_ch=hidden_dim),
+                    ActNorm(3),
+                    AffineCouplingWithCheckerboard(1, hidden_ch=hidden_dim),
+                ]
+            ),
+        ]
+
+        self.channel_transforms = nn.ModuleList(
+            [
+                AffineCouplingWithChannels(True, hidden_ch=hidden_dim),
+                ActNorm(12),
+                AffineCouplingWithChannels(False, hidden_ch=hidden_dim),
+                ActNorm(12),
+                AffineCouplingWithChannels(True, hidden_ch=hidden_dim),
+            ]
+        )
+
+    def forward(self, batch: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        log_det = torch.zeros_like(batch)
+
+        for i, trans in enumerate([self.checker_transforms[0], self.channel_transforms, self.checker_transforms[1]]):
+            for t in trans:
+                batch, d = t(batch)
+                log_det += d
+
+            if i == 0:
+                batch, log_det = squeeze(batch), squeeze(log_det)
+            elif i == 1:
+                batch, log_det = unsqueeze(batch), unsqueeze(log_det)
+
+        return batch, log_det
+
+    def generate(self, z: torch.Tensor) -> torch.Tensor:
+        for i, trans in enumerate([self.checker_transforms[1], self.channel_transforms, self.checker_transforms[0]]):
+            for t in reversed(trans):
+                z, _ = t(z, True)
+
+            if i == 0:
+                z = squeeze(z)
+            elif i == 1:
+                z = unsqueeze(z)
+        return z
+
+
+def squeeze(x: torch.Tensor) -> torch.Tensor:
+    b, c, h, w = x.shape
+    x = x.reshape(b, c, h // 2, 2, w // 2, 2)
+    x = x.permute(0, 1, 3, 5, 2, 4)
+    x = x.reshape(b, c * 4, h // 2, w // 2)
+    return x
+
+
+def unsqueeze(x: torch.Tensor) -> torch.Tensor:
+    b, c, h, w = x.shape
+    x = x.reshape(b, c // 4, 2, 2, h, w)
+    x = x.permute(0, 1, 4, 2, 5, 3)
+    x = x.reshape(b, c // 4, h * 2, w * 2)
+    return x
---
+++ realnvp/train.py
@@ -0,0 +1,117 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+from torch.nn import functional as F
+from torch.distributions import Normal, Uniform
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm, trange
+
+from realnvp.modules import RealNVP
+
+
+class RealNVPTrainer:
+    def __init__(self, device: torch.device, alpha: float = 0.05, hidden_dim: int = 128):
+        self.real_nvp = RealNVP(hidden_dim)
+        self.device = device
+        self.bdist = Normal(torch.tensor([0.0], device=device), torch.tensor([1.0], device=device))
+        self.alpha = alpha
+
+    def log_prob(self, batch: torch.Tensor) -> torch.Tensor:
+        z, log_det = self.real_nvp(batch)
+        p_x = log_det.sum(dim=(1, 2, 3))
+        p_z = self.bdist.log_prob(z).sum(dim=(1, 2, 3))
+        return p_x + p_z
+
+    def preprocess(self, batch: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        batch += Uniform(0, 0.1).sample(batch.shape).to(self.device)
+        batch = batch.clamp(0, 1)
+
+        batch = batch * (1 - self.alpha) + self.alpha
+
+        # NaNs arise so some regularization of log is required
+        # last term is from normalization
+        logit = torch.log(batch + 1e-8) - torch.log(1 - batch + 1e-8) + np.log(1 - self.alpha) - np.log(3)
+        log_det = F.softplus(logit) + F.softplus(-logit)
+
+        return logit, log_det.sum(dim=(1, 2, 3))
+
+    def common_step(self, batch: torch.Tensor) -> torch.Tensor:
+        x, log_det = self.preprocess(batch)
+        log_prob = self.log_prob(x)
+        log_prob += log_det
+        return -log_prob.mean() / (3 * 32 * 32)
+
+    @torch.no_grad()
+    def test(self, test_dataloader: DataLoader) -> np.ndarray:
+        self.real_nvp.eval()
+        losses = []
+        for batch in tqdm(test_dataloader, desc="Testing", leave=False):
+            losses.append(self.common_step(batch).item())
+        return sum(losses) / len(losses)
+
+    def fit(self, train_dataloader: DataLoader, test_dataloader: DataLoader, epochs: int = 10, lr: float = 1e-4):
+        train_losses = []
+        test_losses = [self.test(test_dataloader)]
+
+        optim = AdamW(self.real_nvp.parameters(), lr=lr)
+
+        for _ in (pbar := trange(epochs, desc="Training")):
+            self.real_nvp.train()
+            for i, batch in enumerate(train_dataloader):
+                batch = batch.to(self.device)
+                loss = self.common_step(batch)
+
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+
+                train_losses.append(loss.item())
+                pbar.set_postfix({"loss": train_losses[-1], "batch": f"{i + 1}/{len(train_dataloader)}"})
+
+            test_losses.append(self.test(test_dataloader))
+            tqdm.write(f"Test loss: {test_losses[-1]}")
+
+        return train_losses, test_losses
+
+    @torch.no_grad()
+    def sample2img(self, x: torch.Tensor) -> np.ndarray:
+        x = 1 / (1 + torch.exp(-x))
+        x = x - self.alpha
+        x = x / (1 - self.alpha)
+        x = x.permute(0, 2, 3, 1).clamp(0, 1)
+        return x.cpu().numpy()
+
+    @torch.no_grad()
+    def sample(self, n: int):
+        self.real_nvp.eval()
+        z = self.bdist.sample((n, 3, 32, 32)).squeeze(-1)
+        return self.sample2img(self.real_nvp.generate(z))
+
+    @torch.no_grad()
+    def interpolate(self, images: torch.Tensor) -> np.ndarray:
+        self.real_nvp.eval()
+
+        images = images.to(self.device).float()
+        assert images.shape[0] % 2 == 0
+        start_size = images.shape[0] // 2
+
+        x, _ = self.preprocess(images)
+        z, _ = self.real_nvp(x)
+
+        latents = []
+        for i in range(0, start_size):
+            z_start = z[i].unsqueeze(0)
+            z_finish = z[start_size + i].unsqueeze(0)
+
+            d = (z_finish - z_start) / 5
+
+            latents.append(z_start)
+            for j in range(1, 5):
+                latents.append(z_start + d * j)
+            latents.append(z_finish)
+
+        latents = torch.cat(latents)
+        res = self.real_nvp.generate(latents)
+        return self.sample2img(res)
